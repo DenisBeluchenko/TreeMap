3b1a7f1d0e3b90a4690869d691e64e6015ab0cc0
Denis Beluchenko <dzibis@gmail.com>
2022-11-27 00:29:37 +0700

    final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-
+        List<Person> people = new ArrayList<>();
+        people.add(new Person("Den", "Bul Bul Ogly", 16));
+        people.add(new Person("jula", "Belyuchtnko", 44));
+        people.add(new Person("Any", "Bul Bul Ogly", 46));
+        people.add(new Person("Denis", "Belyuchtnko", 46));
+        Comparator<? super Person> PersonComparator;
+        Collections.sort(people,new PersonComparator());
+        System.out.println(people);
     }
 }
