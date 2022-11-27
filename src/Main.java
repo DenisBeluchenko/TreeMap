733c3478b4d4fe53ef0642869804bc51d725b8fa
Denis Beluchenko <dzibis@gmail.com>
2022-11-27 07:04:58 +0700

    lambda

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,16 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         List<Person> people = new ArrayList<>();
-        people.add(new Person("Den", "Bul Bul Ogly", 16));
-        people.add(new Person("jula", "Belyuchtnko", 44));
+        people.add(new Person("Andrej", "Belyuchtnko", 14));
+        people.add(new Person("jula", "Belyuchtnko", 9));
         people.add(new Person("Any", "Bul Bul Ogly", 46));
         people.add(new Person("Denis", "Belyuchtnko", 46));
-        Comparator<? super Person> PersonComparator;
-        Collections.sort(people,new PersonComparator());
+        Collections.sort(people, compare -> {
+            int len1 = compare.getSurname().split(" ").length;
+            int len2 = compare.getSurname().split(" ").length;
+            if (len1 != len2) return Integer.compare(len1, len2);
+            return Integer.compare(compare.getAge(), compare.getAge());
+        };);
         System.out.println(people);
     }
 }
--- src/PersonComparator.java
+++ src/PersonComparator.java
@@ -5,9 +5,7 @@ public class PersonComparator implements Comparator<Person> {
     public int compare(Person o1, Person o2) {
         int len1=o1.getSurname().split(" ").length;
         int len2=o2.getSurname().split(" ").length;
-        if(len1!= len2){
-            return Integer.compare(len1,len2);
-        }
+        if(len1!= len2) return Integer.compare(len1,len2);
         return Integer.compare(o1.getAge(),o2.getAge());
     }
 }
