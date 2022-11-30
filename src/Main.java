4b63ea23c6b539a974b826293a48ca929e4ad8d4
Denis Beluchenko <dzibis@gmail.com>
2022-11-30 07:08:31 +0700

    final

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         people.add(new Person("jula", "Belyuchtnko", 44));
         people.add(new Person("Any", "Bul Bul Ogly", 46));
         people.add(new Person("Denis", "Belyuchtnko", 46));
-        Collections.sort(people,new PersonComparator());
+        Collections.sort(people, new PersonComparator());
         System.out.println(people);
     }
 }
--- src/PersonComparator.java
+++ src/PersonComparator.java
@@ -3,11 +3,11 @@ import java.util.Comparator;
 public class PersonComparator implements Comparator<Person> {
     @Override
     public int compare(Person o1, Person o2) {
-        int len1=o1.getSurname().split(" ").length;
-        int len2=o2.getSurname().split(" ").length;
-        if(len1!= len2){
-            return Integer.compare(len1,len2);
+        int len1 = o1.getSurname().split(" ").length;
+        int len2 = o2.getSurname().split(" ").length;
+        if (len1 != len2) {
+            return Integer.compare(len1, len2);
         }
-        return Integer.compare(o1.getAge(),o2.getAge());
+        return Integer.compare(o1.getAge(), o2.getAge());
     }
 }
